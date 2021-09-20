64f0289c1045187d8ef43773c3e82f894ba120a5
gucchi4141 <gucchi4141@hotmail.com>
2021-09-20 10:09:36 +0900

    prismを適用したシンプルなプロジェクト

---
+++ prismXamarin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using prismXamarin.Views;
+using prismXamarin.ViewModels;
+using Prism;
+using Prism.Ioc;
+
+
+namespace prismXamarin
+{
+    public partial class App
+    {
+        public App()
+        {
+        }
+        public App(IPlatformInitializer platformInitializer)
+            : base (platformInitializer)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewMode>();
+        }
+    }
+}
---
+++ prismXamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace prismXamarin.ViewModels
+{
+    public class MainPageViewMode : BindableBase, IInitialize, INavigationAware, IDestructible 
+    {
+        protected INavigationService NavigationService { get; private set; }
+
+        public MainPageViewMode(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public virtual void Initialize(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
