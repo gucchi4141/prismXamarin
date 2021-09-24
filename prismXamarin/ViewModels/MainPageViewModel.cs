e3559df11fce66924d26dc9cf46f23a7bced7ce5
gucchi4141 <gucchi4141@hotmail.com>
2021-09-25 08:11:01 +0900

    カメラを使うサンプル

--- prismXamarin/ViewModels/MainPageViewModel.cs
+++ prismXamarin/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Commands;
+
+using Plugin.Media;
+using Xamarin.Forms;
 
 namespace prismXamarin.ViewModels
 {
@@ -10,6 +14,7 @@ namespace prismXamarin.ViewModels
         public MainPageViewMode(INavigationService navigationService)
         {
             NavigationService = navigationService;
+            ButtonPicture = new DelegateCommand(ButtonPicture_Click);
         }
 
         public virtual void Initialize(INavigationParameters parameters)
@@ -31,5 +36,43 @@ namespace prismXamarin.ViewModels
         {
 
         }
+
+        #region 写真
+        private ImageSource _imagePicture = null;
+        public ImageSource ImagePicture
+        {
+            get => _imagePicture;
+            set => SetProperty(ref _imagePicture, value);
+        }
+        #endregion
+
+        #region 撮影ボタン
+        public DelegateCommand ButtonPicture { get; set; }
+        private async void ButtonPicture_Click()
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable ||
+                !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                return;
+            }
+            var mediaOption = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "myPicture",
+                Name = $"{System.DateTime.UtcNow}.jpg"
+            };
+
+            var file = await CrossMedia.Current.TakePhotoAsync(mediaOption);
+            if(null == file)
+            {
+                return;
+            }
+
+            ImagePicture = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+        }
+        #endregion
     }
 }
